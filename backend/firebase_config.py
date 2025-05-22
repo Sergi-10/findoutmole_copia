7ce645a3c80198bafa732173e2bd94ae4d4625a0
Sergio <sergik4x.com>
2025-05-22 11:12:17 +0200

    Comentario en linea en firebase.config.py

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials
 import os
 import json
 
-cred_data = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+cred_data = json.loads(os.environ["FIREBASE_CREDENTIALS"]) #Para otros casos, lo unico que se cambia es REBASE_CREDENTIALS que la variable de entorno que se crea para que la pueda leer Render
 cred = credentials.Certificate(cred_data)
 firebase_admin.initialize_app(cred)
