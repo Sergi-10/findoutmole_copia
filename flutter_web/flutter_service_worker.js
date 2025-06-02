e60a918ec67dddcfdf9bd7adb6d82e2316c6303a
Sergio <sergik4x.com>
2025-06-02 08:45:56 +0200

    nuevo frontend web

--- flutter_web/flutter_service_worker.js
+++ flutter_web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0378b6101babda4c5fb88e3a277f5967
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b9e0079e14865ead3ea6a7294d17b1a8",
+"flutter_bootstrap.js": "79936e3f4234dd29a48b34f6c1b2cbd6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
